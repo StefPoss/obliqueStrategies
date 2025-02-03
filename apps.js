f4acebe47888e53fdc68ddfb808a9e9a310db5fa
lepoSs <7715423+StefPoss@users.noreply.github.com>
2025-02-03 18:34:00 +0100

    add apps.js + txt files in assets/docs

---
+++ apps.js
@@ -0,0 +1,7 @@
+# Oblique Strategies
+# A random sentence generator
+# inspired by Brian Eno and Peter Schmidt's Oblique Strategies
+# written between 1975 and 1979
+
+# Déclarer une tableau de string qui contient toutes les phrases
+# Piocher au hasard une phrase et l'afficher à l'écran sur une page HTML en cliquant sur un bouton
